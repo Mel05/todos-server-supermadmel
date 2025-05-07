ce415f5f390dd9e64db51fe0c2a6527e6ceb5a30
Мельник Дмитрий Сергеевич <34408853+Mel05@users.noreply.github.com>
2025-05-07 18:52:35 +1000

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
 export * as UserController from './UserController.js'
 export * as TodoController from './TodoController.js'
 export * as PostController from './PostController.js'
+
+/// Костыль для onrender
+export * as OnrenderController from './OnrenderController.js'
